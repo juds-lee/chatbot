50a397445b020f96cf460122b541716fbfa3c30d
juds-lee <judy_96_@hotmail.com>
2022-08-24 19:11:29 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const { disconnect } = require("process");
+
+app.use(cors()); 
+
+const server = http.createServer(app); 
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        method: ["GET", "POST"]
+    },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`user with ID ${socket.id} joined room: ${data}`)
+      }) 
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    })
+});
+
+server.listen(3001 , () => {
+    console.log("Server running")
+});

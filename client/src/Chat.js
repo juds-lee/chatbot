6bed15d919d2e28c05e1023518f961ae2cd2fbaf
juds-lee <judy_96_@hotmail.com>
2022-08-24 19:50:00 -0400

    import content

---
+++ client/src/Chat.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {useState} from "react";
+
+function Chat({socket, username, room}) {
+    const [currentMessage, setCurrentMessage] = useState();
+
+    const sendMessage = async () => {
+        if (currentMessage !== "") {
+            const messageData = {
+                room: room,
+                author: username,
+                message: currentMessage,
+                time:
+                new Date(Date.now()).getHours() + 
+                ":" +
+                new Date(Date.now()).getMinutes()  
+            };
+           await socket.emit("send_message", messageData);
+        }
+    }
+
+    return (
+        <div>
+            <div className="chat-header">
+                <p>Live Chat</p>
+            </div>
+            <div className="chat-body">
+
+            </div>
+            <div className="chat-footer">
+             <input
+                type="text"
+                value={currentMessage}
+                placeholder="Hey..."
+                onChange={(event) => {
+                setCurrentMessage(event.target.value);
+                }}
+                onKeyPress={(event) => {
+                event.key === "Enter" && sendMessage();
+              }}
+            />
+                <button onClick={sendMessage}>&#9658;</button>
+            </div>
+        </div>
+    )
+}
+
+export default Chat;
